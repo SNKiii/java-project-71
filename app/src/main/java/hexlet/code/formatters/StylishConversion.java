cc6678cc0681016ea5460ce18e7cfbf6779c3e3a
slei_ghoul228 <ggsanky@gmail.com>
2026-03-26 00:59:34 +0300

    setting style

--- app/src/main/java/hexlet/code/formatters/StylishConversion.java
+++ app/src/main/java/hexlet/code/formatters/StylishConversion.java
@@ -10,14 +10,14 @@ public class StylishConversion {
         String standartFormat = "{";
         for (var obj : list) {
             if (obj.getStatusName().equals(Status.ADDED)) {
-                standartFormat = standartFormat + "\n + " + obj.getKey() + ": " + obj.getOldValue();
+                standartFormat = standartFormat + "\n  + " + obj.getKey() + ": " + obj.getOldValue();
             } else if  (obj.getStatusName().equals(Status.DELETED)) {
-                standartFormat = standartFormat + "\n - " + obj.getKey() + ": " + obj.getOldValue();
+                standartFormat = standartFormat + "\n  - " + obj.getKey() + ": " + obj.getOldValue();
             } else if (obj.getStatusName().equals(Status.UNCHANGED)) {
-                standartFormat = standartFormat + "\n   " + obj.getKey() + ": " + obj.getOldValue();
+                standartFormat = standartFormat + "\n    " + obj.getKey() + ": " + obj.getOldValue();
             } else if (obj.getStatusName().equals(Status.CHANGED)) {
-                standartFormat = standartFormat + "\n - " + obj.getKey() + ": " + obj.getOldValue();
-                standartFormat = standartFormat + "\n + " + obj.getKey() + ": " + obj.getNewValue();
+                standartFormat = standartFormat + "\n  - " + obj.getKey() + ": " + obj.getOldValue();
+                standartFormat = standartFormat + "\n  + " + obj.getKey() + ": " + obj.getNewValue();
             }
         }
         return standartFormat + "\n}";
